00b79b84b70f2ee8b122e738d73a7e76dc4e171e
Jeeermi <134325319+Jeeermi@users.noreply.github.com>
2023-05-24 20:10:35 +0200

    briumap modifica 2

---
+++ data/BriUp_2.js
@@ -0,0 +1,1 @@
+var json_BriUp_2 = {"type":"FeatureCollection","name":"BriUp_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{".":"BriUp"},"geometry":{"type":"Point","coordinates":[9.323776597006047,45.701928498580223]}}]}
